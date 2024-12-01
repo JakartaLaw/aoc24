a0180368b1b927748962a27338a34196ff05431b
JakartaLaw <jeppesjohansen@gmail.com>
2024-12-01 17:42:57 +0100

    second star

--- day1.lean
+++ day1.lean
@@ -31,12 +31,22 @@ match ls with
 list1.zip list2 |>.map (fun (a, b) => (a - b).natAbs)
 | _ => panic! "Expected two lists"
 
-def sumDifferences (l : List Int) : Int :=
+def sumTotal (l : List Int) : Int :=
 l.foldl ( fun acc x => acc + x) 0
+
+def filterAndCount (list1 list2 : List Int) : List Int :=
+  list1.map (fun x : Int => x * (list2.filter (fun (y : Int) => y == x)).length)
+
+def calcSimScore (ls : List (List Int)) : List Int :=
+  match ls with
+  | [list1, list2] => filterAndCount list1 list2
+  | _ => panic! "Expected exactly two lists in input"
 
 def main : IO Unit := do
 let content <- readFile "data/day1.txt"
 let data := transposeData (parseFileContent content)
 let differences := calcDifferences data
-let total_diff := sumDifferences differences
-IO.println s!"File content ¬ {total_diff}"
+let total_diff := sumTotal differences
+IO.println s!"Total difference {total_diff}"
+let sim_score := sumTotal (calcSimScore data)
+IO.println s!"Similarity score {sim_score}"
